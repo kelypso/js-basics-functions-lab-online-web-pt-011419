78b559aa488c31129816bab28ad8bb12d879c6ad
Kelsey Low <kklowlow@gmail.com>
2019-07-24 21:55:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks () {
-  
+
 }
 
 function distanceFromHqInFeet () {
-  
+
 }
 
 function distanceTravelledInFeet () {
-  
+
 }
 
 function calculatesFarePrice () {
